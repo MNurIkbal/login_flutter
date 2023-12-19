d8a150e6c92da768b111402e450c625c8521945f
MNurIkbal <ikbalmnur17@gmail.com>
2023-12-19 10:49:35 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+aplikasi baru di buat menggunakan fluuter 
---
+++ lib/constant.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color whiteColor = Color(0xfffffffff);
+Color textColor = Color(0xff0D0140);
+Color secondaryTextColor = Color(0xff0D0140);
+Color buttonCOlor = Color(0xff130160);
+Color pirmaryButtonColor = Color(0xff130160);
+Color tncButtonColor = Color(0xff130160);
+Color greyColor = Colors.grey;
+Color successColor = Colors.green;
+
+TextStyle whiteTextStyle = GoogleFonts.dmSans(color: whiteColor);
+
+TextStyle textTextStyle = GoogleFonts.dmSans(color: textColor);
+
+TextStyle secondaryTextStyle = GoogleFonts.dmSans(color: secondaryTextColor);
+
+TextStyle tncTextStyle = GoogleFonts.dmSans(color: tncButtonColor);
+
+TextStyle greyTextStyle = GoogleFonts.dmSans(color: greyColor);
+
+TextStyle succescolor = GoogleFonts.dmSans(color: successColor);
+
+FontWeight bold = FontWeight.bold;
